01470c601760af4c3aaaea441d558299f0897eed
Sam Richter <samrichter888@gmail.com>
2024-06-16 09:36:21 -0500

    test keybinds

--- launch/artemis_arena_sim.launch.py
+++ launch/artemis_arena_sim.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
 		PythonLaunchDescriptionSource(
 			os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
 		),
-		launch_arguments={'gz_args': ['-r -s -v4 ', world_path], 'on_exit_shutdown': 'true'}.items()
+		launch_arguments={'gz_args': ['-r -s -v4 ', world_path], 'on_exit_shutdown': 'true', 'pause': 'true'}.items()
 	)
 	# start gazebo client
 	gzclient_cmd = IncludeLaunchDescription(
