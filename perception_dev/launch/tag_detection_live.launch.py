c132693ea2af51026358df048eea5428db8a5395
Sam Richter <samrichter888@gmail.com>
2024-10-27 00:34:06 -0500

    idk

--- perception_dev/launch/tag_detection_live.launch.py
+++ perception_dev/launch/tag_detection_live.launch.py
@@ -3,10 +3,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
-from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.conditions import IfCondition, UnlessCondition
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
 
 
@@ -21,6 +21,8 @@ def make_camera_driver(subdir, is_ip = False):
 
 def generate_launch_description():
 
+    pkg_path = get_package_share_directory('perception_dev')
+
     cameras = [
         ('uvc1', False)
     ]
@@ -34,10 +36,19 @@ def generate_launch_description():
         executable = 'tag_detection_node',
         output = 'screen',
         parameters = [
-            os.path.join(get_package_share_directory('perception_dev'), 'config', 'tag_detection_live.yaml')
+            os.path.join(pkg_path, 'config', 'tag_detection_live.yaml')
             # TODO setup topics based on the array defined here
         ],
         remappings = [ ('tags_detections', '/cardinal_perception/tags_detections') ]
+    ))
+
+    cam_nodes.append(DeclareLaunchArgument('foxglove', default_value = 'false'))
+    cam_nodes.append(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_path, 'launch', 'foxglove.launch.py')
+        ),
+        launch_arguments = {'use_sim_time': 'false'}.items(),
+        condition = IfCondition(LaunchConfiguration('foxglove', default='false'))
     ))
 
     return LaunchDescription(cam_nodes)
